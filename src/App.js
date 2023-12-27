f199ee9dc568b1745f36531a2e56ae13b9b14b7c
Nakul <nakul@Harshits-MacBook-Pro-2.local>
2023-12-28 00:13:14 +0530

    hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './components/Home.tsx';
-import { BrowserRouter as Router,Routes ,Route } from 'react-router-dom';
+import { Routes ,Route } from 'react-router-dom';
 import Movies from './components/Movies.tsx';
 import Series from './components/Series.tsx';
 import NewPop from './components/NewPop.tsx';
@@ -8,14 +8,12 @@ import "react-activity/dist/library.css";
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/movies" element={<Movies/>} />
         <Route path="/series" element={<Series/>} />
         <Route path="/new&popular" element={<NewPop/>} />
       </Routes>
-    </Router>
   );
 }
 
